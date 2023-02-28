772f0183962249af0464e031d9c18b7995863bb3
Giovanny Arias <giovannyarias@Giovannys-MacBook-Pro.local>
2023-02-28 19:28:38 +0100

    height test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-const documentHeight = () => {
-  const doc = document.documentElement
-  doc.style.setProperty("--doc-height", `${window.innerHeight}px`)
-}
-window.addEventListener("resize", documentHeight)
-documentHeight()
-
 export default function Home() {
   return (
     <main className={styles.main}>
@@ -15,3 +8,4 @@ export default function Home() {
     </main>
   )
 }
+
