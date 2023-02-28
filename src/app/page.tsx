95e43816b5b30abda182b65db9dd7ebf6508de3d
Giovanny Arias <giovannyarias@Giovannys-MacBook-Pro.local>
2023-02-28 19:36:58 +0100

    height test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import Image from 'next/image'
+import { useEffect } from 'react';
 import styles from './page.module.css'
 
 export default function Home() {
+
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, [])
+
   return (
     <main className={styles.main}>
       Giovanny Arias
