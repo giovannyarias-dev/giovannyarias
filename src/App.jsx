eb8b35290633f5c97203068f9efe992357b740d2
Giovany Arias <giovannyarias.developer@gmail.com>
2024-02-25 13:33:58 +0100

    🚧 Add glasses svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import { useEffect, useState } from 'react'
 import { animate, motion, useMotionValue, useTransform } from "framer-motion";
 import './App.css'
-import { leftFrameLogo, leftFrame } from './paths/paths';
+import { leftFrameLogo, leftFrame, leftLens, leftLensLogo } from './paths/paths';
 import { getIndex, useFlubber } from './use-flubber';
 
-const paths = [leftFrame, leftFrameLogo, leftFrame];
+const framePaths = [leftFrame, leftFrameLogo, leftFrame];
+const lensPaths = [leftLens, leftLensLogo, leftLens];
 const colors = ["#00cc88", "#0099ff", "#00cc88"];
 
 function App() {
   const [pathIndex, setPathIndex] = useState(0);
   const progress = useMotionValue(pathIndex);
-  const fill = useTransform(progress, paths.map(getIndex), colors);
-  const path = useFlubber(progress, paths);
+  const fill = useTransform(progress, framePaths.map(getIndex), colors);
+  const framePath = useFlubber(progress, framePaths);
+
+  const lensPath = useFlubber(progress, lensPaths);
+
 
   useEffect(() => {
     const animation = animate(progress, pathIndex, {
       duration: 3,
       ease: "easeInOut",
       onComplete: () => {
-        if (pathIndex === paths.length - 1) {
+        if (pathIndex === framePaths.length - 1) {
           progress.set(0);
           setPathIndex(1);
         } else {
@@ -31,11 +35,14 @@ function App() {
   }, [pathIndex]);
 
   return (
-    <svg width="400" height="400">
-      <g transform="scale(10 10)">
-        <motion.path fill={fill} d={path} />
-      </g>
-    </svg>
+    <>
+      <svg width="200" height="200">
+        <g transform="scale(10 10)">
+          <motion.path fill={fill} d={framePath} />
+          <motion.path fill={'#242424'} d={lensPath} />
+        </g>
+      </svg>
+    </>
   );
 }
 
