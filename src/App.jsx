cf7f287f305a413ffc3b54e62278a3a7285e5547
Giovany Arias <giovannyarias.developer@gmail.com>
2024-02-29 22:26:44 +0100

    🚧 Complete glasses animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import { useEffect, useState } from 'react'
 import { animate, motion, useMotionValue, useTransform } from "framer-motion";
 import './App.css'
-import { leftFrameLogo, leftFrame, leftLens, leftLensLogo } from './paths/paths';
+import { leftFrameLogo, leftFrame, leftLens, leftLensLogo, rightFrame, rightFrameLogo, rightLens, rightLensLogo } from './paths/paths';
 import { getIndex, useFlubber } from './use-flubber';
 
 const framePaths = [leftFrame, leftFrameLogo, leftFrame];
 const lensPaths = [leftLens, leftLensLogo, leftLens];
+
+const rightFramePaths = [rightFrame, rightFrameLogo, rightFrame];
+const rightLensPaths = [rightLens, rightLensLogo, rightLens];
+
 const colors = ["#00cc88", "#0099ff", "#00cc88"];
 
 function App() {
   const [pathIndex, setPathIndex] = useState(0);
   const progress = useMotionValue(pathIndex);
   const fill = useTransform(progress, framePaths.map(getIndex), colors);
+  
   const framePath = useFlubber(progress, framePaths);
-
   const lensPath = useFlubber(progress, lensPaths);
+
+  const rightFramePath = useFlubber(progress, rightFramePaths);
+  const rightLensPath = useFlubber(progress, rightLensPaths);
+
 
 
   useEffect(() => {
@@ -40,6 +48,8 @@ function App() {
         <g transform="scale(10 10)">
           <motion.path fill={fill} d={framePath} />
           <motion.path fill={'#242424'} d={lensPath} />
+          <motion.path fill={fill} d={rightFramePath} />
+          <motion.path fill={'#242424'} d={rightLensPath} />
         </g>
       </svg>
     </>
